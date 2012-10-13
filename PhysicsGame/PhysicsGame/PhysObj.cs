eafa9d9fdbed19e997c60535a217522451cd55ff
Colin Maykish <crmaykish@gmail.com>
2012-10-13 15:04:38 -0400

    added TextureFactory and GenerateCircleTexture method

---
+++ PhysicsGame/PhysicsGame/PhysObj.cs
@@ -0,0 +1,28 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace PhysicsGame
+{
+    class PhysObj
+    {
+        #region Physics Constants
+        float gravity = 0.3f;
+        float hFriction = -0.04f;
+        float elasticity = 0.6f;
+        #endregion
+
+        Vector2 Position;
+        Vector2 Velocity;
+        Vector2 Mass;
+    }
+}
---
+++ PhysicsGame/PhysicsGame/TextureFactory.cs
@@ -0,0 +1,48 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace PhysicsGame
+{
+    class TextureFactory
+    {
+        GraphicsDevice GDevice;
+
+        public TextureFactory(GraphicsDevice device)
+        {
+            GDevice = device;
+        }
+
+        public Texture2D GenerateCircleTexture(int radius, Color color)
+        {
+            int diameter = radius*2;
+
+            Texture2D circle = new Texture2D(GDevice, diameter, diameter);
+            Color[] data = new Color[diameter * diameter];
+            Vector2 center = new Vector2(radius, radius);
+
+            for (int i = 0; i < diameter; i++)
+            {
+                for (int j = 0; j < diameter; j++)
+                {
+                    Vector2 pt = new Vector2(i, j);
+                    if ((center - pt).Length() < radius)
+                    {
+                        data[diameter * i + j] = Color.White;
+                    }
+                }
+            }
+            circle.SetData(data);
+            return circle;
+        }
+    }
+}
